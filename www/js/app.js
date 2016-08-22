2b3330f60e1adc9f40ecbe7dad84e2fd96a20791
Alex Anthony <alexanderanthony813@gmail.com>
2016-08-21 17:31:26 -0700

    API integrated.

---
+++ www/js/app.js
@@ -0,0 +1,31 @@
+angular
+  .module('myapp', ['algoliasearch'])
+  .controller('SearchCtrl', ['$scope', 'algolia', function($scope, algolia) {
+
+    $scope.search = {
+      query: 'Asus',
+      hits: []
+    };
+    var client = algolia.Client('A9EJIIWSXQ', '0ddfdbe577785a0697254cec4e2e7197');
+    var index = client.initIndex('best_buy');
+
+    index.search($scope.search.query)
+        .then(function searchSuccess(content) {
+          console.log(content);
+          // add content of search results to scope for display in view
+          $scope.search.hits = content.hits;
+        }, function searchFailure(err) {
+          console.log(err);
+      });
+
+    $scope.$watch('search.query', function() {
+      index.search($scope.search.query)
+        .then(function searchSuccess(content) {
+          console.log(content);
+          // add content of search results to scope for display in view
+          $scope.search.hits = content.hits;
+        }, function searchFailure(err) {
+          console.log(err);
+      });
+    });
+  }]);
