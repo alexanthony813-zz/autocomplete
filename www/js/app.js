999e809c33b8c404843859eea00632ee35a4d8e9
Alex Anthony <alexanderanthony813@gmail.com>
2016-08-21 20:53:20 -0700

    Search feature updated

--- www/js/app.js
+++ www/js/app.js
@@ -1,15 +1,19 @@
 angular
   .module('myapp', ['algoliasearch'])
   .controller('SearchCtrl', ['$scope', 'algolia', function($scope, algolia) {
+    var query;
 
     $scope.search = {
-      query: '',
       hits: []
     };
+
     var client = algolia.Client('A9EJIIWSXQ', '0ddfdbe577785a0697254cec4e2e7197');
     var index = client.initIndex('best_buy');
 
-    index.search($scope.search.query)
+    $scope.update = function(){
+      console.log(document.getElementById("search-input").value)
+      query = document.getElementById("search-input").value;
+      index.search(query)
         .then(function searchSuccess(content) {
           console.log(content);
           // add content of search results to scope for display in view
@@ -17,32 +21,19 @@ angular
         }, function searchFailure(err) {
           console.log(err);
       });
-
-    $scope.$watch('search.query', function() {
-      index.search($scope.search.query)
-        .then(function searchSuccess(content) {
-          console.log(content);
-          // add content of search results to scope for display in view
-          $scope.search.hits = content.hits;
-        }, function searchFailure(err) {
-          console.log(err);
-      });
-    });
-
-    //
+    }
+    //autocomplete.js
     autocomplete('#search-input', {hint: false}, [
-    {
-      source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
-      displayKey: 'name',
-      templates: {
-        suggestion: function(suggestion) {
-          return suggestion.name;
+      {
+        source: autocomplete.sources.hits(index, {hitsPerPage: 5}),
+        displayKey: 'name',
+        templates: {
+          suggestion: function(suggestion) {
+            return suggestion.name;
+          }
         }
       }
-    }
     ]).on('autocomplete:selected', function(event, suggestion, dataset) {
-      console.log(suggestion.name)
       $scope.search.query = suggestion.name;
-
     });
   }]);
